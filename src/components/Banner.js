8b0acce2d55f84ffb9145eff07a8aabc319470f5
Pearly3 <ben.pearl@btinternet.com>
2025-02-19 16:37:11 +0000

    banner with animation in

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,28 +1,59 @@
 import { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap"
+
+import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import imgHeader from '../assets/img/ZoroHeader.png'
 
 export const Banner = () => {
-    const [numLoop, setnumLoop] = useState(0);
+    const [text, setText] = useState('');
+    const [numLoop, setNumLoop] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const wordRotate = ["Full-stack Software Developer", "Data Engineer", "Cloud Engineer" ];
-    const []
+    const wordRotate = ["Full-Stack Developer", "Data Engineer", "Cloud Engineer" ];
+    const period = 2000;
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+    
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+
+        }, delta)
+        return () => {clearInterval(ticker)}
+    }, [text])
+
+    const tick = () => {
+        let i = numLoop % wordRotate.length;
+        let fullText = wordRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+        
+        setText(updatedText);
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if(isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setNumLoop(numLoop + 1);
+            setDelta(500);
+        }
+    }
     return (
-        <section className = "Banner" id="home">
+        <section className = "banner" id="home">
             <Container>
                 <Row className="align-items-center">
                    <Col xs={12} md={6} xl={7}>
                      <span className="tagline">Welcome to my Portfolio</span>
-                     <h1>{'Hi I\'m Ben Pearl'}<span className="wrap">web developer</span></h1>
+                     <h1>{'Hi I\'m Ben Pearl '}<span className="wrap">{text}</span></h1>
                      <p> I am an ambitious, flexible, and personable graduate with a highly analytical mindset. Committed to continuous learning through a passion for technology and software by expanding knowledge of relevant technologies and their subsequent applications. A natural team player, able to drive efficiency by focusing on strategies which achieve results. My experience with sales and outreach has sharpened my already strong communication and collaboration skills and combined with my eagerness to learn and exceptional problem solving skills, I am confident and excited to contribute to an ambitious technology team. I recently completed the Makers software development bootcamp which allowed me to sharpen my ability as a software developer.</p>
                      <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
 
                    
                    </Col>
-                   <col xs={12} md ={6} xl={5}>
-                     <img scr={imgHeader} alt="Header Img" />
-                   </col>
+                   <Col xs={12} md ={6} xl={5}>
+                     <img src={imgHeader} alt="Header Img" />
+                   </Col>
                 </Row>
             </Container>
         </section>
