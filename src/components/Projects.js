4b82c1c689ea2fb5953755ade44f53205a57d25c
Pearly3 <ben.pearl@btinternet.com>
2025-02-20 16:04:34 +0000

    projects in app structure + base structuring

---
+++ src/components/Projects.js
@@ -0,0 +1,18 @@
+
+import { Container, Col, Row, Nav } from "react-bootstrap";
+
+export const Projects = () => {
+
+
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
