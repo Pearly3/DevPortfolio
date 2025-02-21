1143d72cab2dc8f03262d33f9f3f2e74a132f617
Pearly3 <ben.pearl@btinternet.com>
2025-02-21 15:55:56 +0000

    blog stuff with mongoDB info

--- server.js
+++ server.js
@@ -3,12 +3,40 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const connectDB = require('./src/config/db');
+const Post = require('./src/models/Post');
+
+// Connect to MongoDB
+connectDB();
 
 // server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
+app.get('/api/posts', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/api/posts', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
