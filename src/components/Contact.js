c8fb5f953e047aba539533bff173deb8f55cca10
Pearly3 <ben.pearl@btinternet.com>
2025-02-21 12:15:13 +0000

    form code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,6 +22,8 @@ export const Contact = () => {
         })
     }
 
+    
+
     return (
         <section className="contact" id="connect">
             <Container>
@@ -38,7 +40,27 @@ export const Contact = () => {
                             </Col>
                             <Col sm={6} className="px-1">
                             
+                            <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                             </Col>
+                            <Col sm={6} className="px-1">
+                            
+                            <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)}/>
+                            </Col>
+                            <Col sm={6} className="px-1">
+                            
+                            <input type="tel" value={formDetails.phone} placeholder="Phone" onChange={(e) => onFormUpdate('phoe', e.target.value)}/>
+                            </Col>
+                            <Col>
+                              <textarea row="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}/>
+                              <button type="submit"><span>{buttonText}</span></button>
+                            </Col>
+                            {
+                                status.nessage &&
+                                <Col>
+                                  <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                                </Col>
+                            }
+
                         </Row>
                       </form>
                     </Col>
